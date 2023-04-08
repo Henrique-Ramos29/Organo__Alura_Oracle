e80a96923a8ca91e81d45ace706dbc4ebd4aff6e
Henrique-Ramos29 <120992364+Henrique-Ramos29@users.noreply.github.com>
2023-04-07 22:20:35 -0300

    rodape

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from 'react'
+import './Rodape.css'
+
+export const Rodape = () => {
+    return (
+
+
+        <footer className='rodape'>
+            <>
+                <img src='/imagens/logo.png' alt='logo' />
+                <p>Desenvolvido Henrique + Alura + Oracle</p>
+            </>
+        </footer>
+
+    )
+}
+
