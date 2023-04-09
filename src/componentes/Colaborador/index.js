389e86fa39ba384e88365fde6445d18d36571f1c
Henrique-Ramos29 <120992364+Henrique-Ramos29@users.noreply.github.com>
2023-04-08 22:46:21 -0300

    Atualização

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,17 +1,35 @@
-import './Colaborador.css'
 
-const Colaborador = ({ imagem, nome, cargo, corDeFundo }) => {
-    return (
-        <div className='colaborador'>
-            <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
-                <img src={imagem} alt={nome} />
-            </div>
-            <div className='rodape'>
-                <h4>{nome}</h4>
-                <h5>{cargo}</h5>
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import './colaborador.css'
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+    return (<div className="colaborador">
+        <AiFillCloseCircle
+            size={25}
+            className='deletar'
+            onClick={() => aoDeletar(colaborador.id)} />
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
+        </div>
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito
+                    ? <AiFillHeart { ...propsfavorito} color='red' />
+                    : <AiOutlineHeart { ...propsfavorito} />
+                }
             </div>
         </div>
-    )
+    </div>)
 }
 
 export default Colaborador
--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,17 +1,36 @@
-import { Fragment } from 'react'
-import './Rodape.css'
 
-export const Rodape = () => {
-    return (
+import './rodape.css'
 
-
-        <footer className='rodape'>
-            <>
-                <img src='/imagens/logo.png' alt='logo' />
-                <p>Desenvolvido Henrique + Alura + Oracle</p>
-            </>
-        </footer>
-
-    )
+const Rodape = () => {
+    return (<footer className="footer">
+       <section>
+       <ul>
+            <li>
+                <a href="facebook.com" target="_blank">
+                    <img src="/imagens/facebook.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="twitter.com" target="_blank">
+                    <img src="/imagens/twitter.png" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="instagram.com" target="_blank">
+                    <img src="/imagens/instagram.png" alt="" />
+                </a>
+            </li>
+        </ul>
+       </section>
+       <section>
+       <img src="/imagens/logo.png" alt="" />
+       </section>
+       <section>
+        <p>
+            Desenvolvido por Henrique + Alura.
+        </p>
+       </section>
+    </footer>)
 }
 
+export default Rodape
